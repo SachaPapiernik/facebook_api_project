2e83f4d446f7f0cbb87fd4072f2c8825f9c245d2
SachaPapiernik <sacha.papiernik@gmail.com>
2023-11-24 18:43:58 +0100

    added user endpoint

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
+import pandas as pd
 
 app = FastAPI()
 
@@ -31,7 +32,7 @@ async def create_event(event: EventCreate):
     global event_id
     new_event = Event(**event.model_dump(), id=event_id)
     event_id += 1
-    db.append(new_event)
+    db_event.append(new_event)
     return new_event
 
 @app.get("/events/{event_id}", response_model=Event)
@@ -63,24 +64,59 @@ class GroupCreate(GroupBase):
 class Group(GroupBase):
     id: int
 
-db_group = []
+db_group_df = pd.DataFrame(columns=["id", "name", "description", "icon", "cover_photo", "group_type", "allow_members_to_publish", "allow_members_to_create_events"])
 group_id = 1
 
 @app.post("/groups/", response_model=Group)
 async def create_group(group: GroupCreate):
     global group_id
-    new_group = Group(**group.model_dump(), id=group_id)
+    new_group_data = {"id": group_id, **group.model_dump()}
+    db_group_df.loc[group_id] = new_group_data
     group_id += 1
-    db_group.append(new_group)
-    return new_group
+    return new_group_data
 
 @app.get("/groups/{group_id}", response_model=Group)
 async def read_group(group_id: int):
-    group = next((g for g in db_group if g.id == group_id), None)
-    if group is None:
+    try:
+        group_data = db_group_df.loc[group_id].to_dict()
+        return group_data
+    except: 
         raise HTTPException(status_code=404, detail="Group not found")
-    return group
-
+    
 @app.get("/groups/", response_model=List[Group])
 async def read_groups(skip: int = 0, limit: int = 10):
-    return db_group[skip : skip + limit]
+    return db_group_df.iloc[skip : skip + limit].to_dict(orient="records")
+
+# User Models
+class UserBase(BaseModel):
+    name: str
+    email: str
+
+class UserCreate(UserBase):
+    pass
+
+class User(UserBase):
+    id: int
+
+db_user_df = pd.DataFrame(columns=["id", "name", "email"])
+user_id = 1
+
+@app.post("/users/", response_model=User)
+async def create_user(user: UserCreate):
+    global user_id
+    new_user_data = {"id": user_id, **user.model_dump()}
+    db_user_df.loc[user_id] = new_user_data
+    user_id += 1
+    return new_user_data
+
+@app.get("/users/{user_id}", response_model=User)
+async def read_user(user_id: int):
+    try:
+        user_data = db_user_df.loc[user_id].to_dict()
+        return user_data
+    except:
+        raise HTTPException(status_code=404, detail="User not found")
+
+@app.get("/users/", response_model=List[User])
+async def read_users(skip: int = 0, limit: int = 10):
+    return db_user_df.iloc[skip : skip + limit].to_dict(orient="records")
