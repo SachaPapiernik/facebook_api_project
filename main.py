07d914ef4aca84280bd6c85ce0d0cdf585c4f2de
SachaPapiernik <sacha.papiernik@gmail.com>
2023-11-24 21:45:37 +0100

    added thread and message endpoint

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
 import pandas as pd
+from datetime import datetime
 
 app = FastAPI()
 
-#Event
+# Event
 
 class EventBase(BaseModel):
     name: str
@@ -47,7 +48,7 @@ async def read_events(skip: int = 0, limit: int = 10):
     return db_event[skip: skip + limit]
 
 
-#Group
+# Group
 
 class GroupBase(BaseModel):
     name: str
@@ -57,6 +58,8 @@ class GroupBase(BaseModel):
     group_type: str  # Assuming group_type can be 'public', 'private', or 'secret'
     allow_members_to_publish: bool
     allow_members_to_create_events: bool
+    admin: List[str] = []
+    members: List[str] = []
 
 class GroupCreate(GroupBase):
     pass
@@ -64,7 +67,7 @@ class GroupCreate(GroupBase):
 class Group(GroupBase):
     id: int
 
-db_group_df = pd.DataFrame(columns=["id", "name", "description", "icon", "cover_photo", "group_type", "allow_members_to_publish", "allow_members_to_create_events"])
+db_group_df = pd.DataFrame(columns=["id", "name", "description", "icon", "cover_photo", "group_type", "allow_members_to_publish", "allow_members_to_create_events", "admin", "members"])
 group_id = 1
 
 @app.post("/groups/", response_model=Group)
@@ -87,7 +90,8 @@ async def read_group(group_id: int):
 async def read_groups(skip: int = 0, limit: int = 10):
     return db_group_df.iloc[skip : skip + limit].to_dict(orient="records")
 
-# User Models
+# User
+
 class UserBase(BaseModel):
     name: str
     email: str
@@ -120,3 +124,69 @@ async def read_user(user_id: int):
 @app.get("/users/", response_model=List[User])
 async def read_users(skip: int = 0, limit: int = 10):
     return db_user_df.iloc[skip : skip + limit].to_dict(orient="records")
+
+class MessageBase(BaseModel):
+    text: str
+    user: str
+    timestamp: datetime
+
+class MessageCreate(MessageBase):
+    pass
+
+class Message(MessageBase):
+    id: str
+
+class ThreadBase(BaseModel):
+    text: str
+    user: str
+    timestamp: datetime
+    messages: List[Message] = []
+
+class ThreadCreate(ThreadBase):
+    pass
+
+class Thread(ThreadBase):
+    id: str
+
+db_threads = {}
+thread_id = 1
+
+@app.post("/threads/", response_model=Thread)
+async def create_thread(thread: ThreadCreate):
+    global thread_id
+    new_thread_data = {"id": f"{thread_id}", **thread.model_dump()}
+    db_threads[f"{thread_id}"] = new_thread_data
+    thread_id += 1
+    return Thread(**new_thread_data)
+
+@app.get("/threads/{thread_id}", response_model=Thread)
+async def read_thread(thread_id: str):
+    thread_data = db_threads.get(thread_id)
+    if thread_data is None:
+        raise HTTPException(status_code=404, detail="Thread not found")
+    return thread_data
+
+@app.get("/threads/{thread_id}/messages/{message_id}", response_model=Message)
+async def read_message_in_thread(thread_id: str, message_id: str):
+    thread_data = db_threads.get(thread_id)
+    if thread_data is None:
+        raise HTTPException(status_code=404, detail="Thread not found")
+
+    message_data = next((msg for msg in thread_data["messages"] if msg["id"] == message_id), None)
+    if message_data is None:
+        raise HTTPException(status_code=404, detail="Message not found in thread")
+
+    return message_data
+
+@app.post("/threads/{thread_id}/messages/", response_model=Message)
+async def create_message_in_thread(thread_id: str, message: MessageCreate):
+    thread_data = db_threads.get(thread_id)
+    if thread_data is None:
+        raise HTTPException(status_code=404, detail="Thread not found")
+
+    new_message_data = {
+        "id": f"{len(thread_data['messages']) + 1}",
+        **message.model_dump(),
+    }
+    thread_data["messages"].append(new_message_data)
+    return new_message_data
